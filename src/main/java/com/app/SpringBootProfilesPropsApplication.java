6f15305b520c34463c290ca2a143c09e2fa59bdc
Admin <Admin@192.168.0.7>
2019-06-09 19:23:25 +0530

    ProfilesProps Commit

---
+++ src/main/java/com/app/SpringBootProfilesPropsApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProfilesPropsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProfilesPropsApplication.class, args);
+	}
+
+}
